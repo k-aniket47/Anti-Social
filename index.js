aaf81f94e1fe59673a89346d031c6739da875668
Aniket Kumar <aniketch345@gmail.com>
2022-07-11 00:24:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 8888
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Server is running on port ${port}!`))
