7bc6ed5da8ab213071b17ec01712b7e83f302c37
Aniket Kumar <aniketch345@gmail.com>
2022-07-11 18:33:52 +0530

    sign in and up

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+const userschema =new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    }
+
+},
+{timestamp:true})
+
+const User=mongoose.model('user',userschema)
+
+module.require=User;
