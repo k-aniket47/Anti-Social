a08e23c385f4ca8f8f687f9f063833c283851a73
Aniket Kumar <aniketch345@gmail.com>
2022-07-13 22:53:00 +0530

    till post

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const express = require('express')
 const cookieParser = require('cookie-parser');
 const app = express()
 const port = 8888
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport')
 const passportlocal=require('./config/passport-local-strategy')
 const MongoStore = require('connect-mongo');
+const sassMiddleware =require('node-sass-middleware')
+
+app.use(sassMiddleware({
+    src: './static/sass',
+    dest: './static/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix:  '/css' 
+}));
 
 app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('static'));
 
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 app.set('view engine','ejs')
