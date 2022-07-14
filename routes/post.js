ac333b954a37b5ffe867334a71ed6fd2c108f5e0
Aniket Kumar <aniketch345@gmail.com>
2022-07-14 18:40:59 +0530

    full post

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const router=express.Router()
+const passport=require('passport')
 const postController=require('../controllers/postcontroller')
 
-router.post('/create',postController.create)
+
+router.post('/create',passport.checkAuthentication ,postController.create)
 
 module.exports= router;
