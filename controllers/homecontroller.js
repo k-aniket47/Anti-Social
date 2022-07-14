377dd1dd817f32a9b6f41e8d64c770b1c8b2a259
Aniket Kumar <aniketch345@gmail.com>
2022-07-14 18:31:57 +0530

    post till authentication

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,5 +1,20 @@
+const Post =require('../models/post')
 module.exports.home= function(req,res){
-    return res.render('home',{
-        "title":"Anti Social"
+    // Post.find({},function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
+    // })
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        if (err) {
+            console.log("error in populating")
+            return
+        }
+        return res.render('home', {
+            title: "Anti Social | Home",
+            posts:  posts
+        });
     })
 }
--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const postSchema= new mongoose.Schema({
     },
     user:{
         type: mongoose.Schema.Types.ObjectId,
-        ref:'User'
+        ref:'user'
     }
 
 },{
